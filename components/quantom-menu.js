c587b22aad276dd854da721859cdd96063223d44
Parking Swing <148919566+parkingswing2323@users.noreply.github.com>
2023-12-10 13:10:06 +0100

    Update quantom-menu.js

--- components/quantom-menu.js
+++ components/quantom-menu.js
@@ -32,7 +32,7 @@ const QuantomMenu = ({ onClose }) => {
   }, []);
 
   const onFrameButton2Click = useCallback(() => {
-    window.open("https://jeffspace.vercel.app");
+    window.open("https://parkingswing.vercel.app");
   }, []);
 
   return (

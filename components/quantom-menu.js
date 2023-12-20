dfff7c97575a4ef4604c5f6437fd674d0389b82f
Jeff <148919566+jeff96655@users.noreply.github.com>
2023-12-20 10:32:36 +0100

    Update quantom-menu.js

--- components/quantom-menu.js
+++ components/quantom-menu.js
@@ -32,7 +32,7 @@ const QuantomMenu = ({ onClose }) => {
   }, []);
 
   const onFrameButton2Click = useCallback(() => {
-    window.open("https://parkingswing.vercel.app");
+    window.open("https://jeffspace.vercel.app");
   }, []);
 
   return (
